620fa06541f4b9e2c6ac4883982e9f025a7c7e63
anugerahc <anugerahchristian@gmail.com>
2023-02-13 22:45:39 +0700

    authentication implemented

--- src/server.js
+++ src/server.js
@@ -1,16 +1,28 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
+
+// Notes Plugin
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/notesService');
 const {NotesValidator} = require('./validator/notes');
+
+// User Plugin
 const users = require('./api/users');
 const UsersService = require('./services/postgres/usersService');
 const {UsersValidator} = require('./validator/users');
 
+// Auth Plugin
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/authenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
+const {AuthenticationsValidator} = require('./validator/authentications');
+
 const init = async () => {
   const notesService = new NotesService();
   const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -19,6 +31,28 @@ const init = async () => {
         origin: ['*'],
       },
     },
+  });
+
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  server.auth.strategy('notesapp_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      Credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   });
 
   await server.register([
@@ -34,6 +68,15 @@ const init = async () => {
       options: {
         service: usersService,
         validator: UsersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
       },
     },
   ]);
